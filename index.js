65cf12d680ab77023884bdfd8419f0e937240d0d
Kyle Mitchell <kyle@kemitchell.com>
2015-07-15 16:29:22 -0700

    Rename distance limit variable

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var leven = require('leven')
 var Transform = require('stream').Transform
 require('util').inherits(LevenFilter, Transform)
 
-function LevenFilter(string, threshold) {
+function LevenFilter(string, maximumDistance) {
   if (!(this instanceof LevenFilter)) {
-    return new LevenFilter(string, threshold) }
+    return new LevenFilter(string, maximumDistance) }
   this.string = string
-  this.threshold = threshold
+  this.maximumDistance = maximumDistance
   Transform.call(this, { objectMode: true }) }
 
 LevenFilter.prototype._transform = function(chunk, encoding, done) {
   chunk = typeof chunk === 'string' ? chunk : chunk.toString()
-  if (leven(chunk, this.string) <= this.threshold) {
+  if (leven(chunk, this.string) <= this.maximumDistance) {
     this.push(chunk) }
   done() }
 
